c1cec35781d70d9d17b50aae217fbb0cf24c07ca
Nick Hingston <nick@wearemothership.com>
2018-09-08 13:36:24 +0100

    support MONOCHROME1

--- src/image.js
+++ src/image.js
@@ -613,8 +613,10 @@ daikon.Image.prototype.getInterpretedData = function (asArray, asObject, frameIn
         }
     }
     
-    var lutShape = daikon.Image.getSingleValueSafely(this.getTag(daikon.Tag.TAG_LUT_SHAPE[0], daikon.Tag.TAG_LUT_SHAPE[1]), 0);
-    if (lutShape === "INVERSE") {
+    // invert pixel values if INVERTED xor MONOCHROME1
+    var invert = daikon.Image.getSingleValueSafely(this.getTag(daikon.Tag.TAG_LUT_SHAPE[0], daikon.Tag.TAG_LUT_SHAPE[1]), 0) === "INVERSE";
+    invert = !invert && this.getPhotometricInterpretation() === "MONOCHROME1";
+    if (invert) {
         var maxVal = Math.pow(2, this.getBitsStored());
         if (datatype === daikon.Image.BYTE_TYPE_INTEGER) {
             maxVal /= 2;
